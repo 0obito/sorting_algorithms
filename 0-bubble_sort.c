0bffd0ebb89cc56950fe42f522892fc9c1cfb497
0obito <elmsafer.abdellah00@gmail.com>
2024-01-19 19:33:45 +0100

    lil changes

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "sort.h"
 
 /**
@@ -13,14 +12,14 @@ void bubble_sort(int *array, size_t size)
 	int temp;
 	size_t i, j;
 
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
-		for(j = 0; j < size - 1 - i; j++)
+		for (j = 0; j < size - 1 - i; j++)
 		{
-			if(array[j] > array[j+1])
+			if (array[j] > array[j + 1])
 			{
-				temp = array[j+1];
-				array[j+1] = array[j];
+				temp = array[j + 1];
+				array[j + 1] = array[j];
 				array[j] = temp;
 				print_array(array, size);
 			}
